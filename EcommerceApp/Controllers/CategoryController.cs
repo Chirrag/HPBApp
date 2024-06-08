89efd9a511340402d883c29debdcc6416673f5bf
Chirag Joshi <81093475+Chirrag@users.noreply.github.com>
2024-06-09 01:26:33 +0530

    initial commit

---
+++ EcommerceApp/Controllers/CategoryController.cs
@@ -0,0 +1,22 @@
+﻿using EcommerceApp.Data;
+using EcommerceApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EcommerceApp.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CategoryController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            List<Category>objCategoryList = _context.Categories.ToList();
+            return View(objCategoryList);
+        }
+    }
+}
